0a2ba45ac48085ef075431641e6844b95977589f
niyorn <c.niyornram@hotmail.com>
2019-08-13 14:28:35 +0200

    add: Content to body

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,41 @@
 <template>
   <Layout>
     
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <section v-for="vraag in $page.vragen.edges" :key="vraag.id">
+      <vraag
+        :title="vraag.node.title"
+        :image="vraag.node.featuredImage"
+        :date="vraag.node.date"
+        :content="vraag.node.content"
+      />
+    </section>
   </Layout>
 </template>
 
+<page-query>
+  query Posts {
+    vragen: allContent{
+      edges {
+        node {
+          id
+          path
+          title
+          date
+          content
+          featuredImage
+        }
+      }
+    }
+  }
+</page-query>
+
+
 <script>
+import vraag from "@/templates/vraag"
 export default {
+  components: {
+    vraag
+  },
   metaInfo: {
     title: 'Hello, world!'
   }
---
+++ src/templates/vraag.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <h2>{{title}}</h2>
+        <img :src="image" alt="">
+        <span>{{date}}</span>
+        <p>{{content}}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "vraag",
+    props: {
+        title: String,
+        image: String,
+        date: String,
+        content: String
+    }
+}
+</script>
+
+<style>
+
+</style>
