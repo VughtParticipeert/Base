db2601739694441e374570a24e1a635135560520
niyorn <c.niyornram@hotmail.com>
2019-08-19 09:07:11 +0200

    fix: Navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -118,6 +118,7 @@
                 background: white;
                 height: 100%;
                 width: 100%;
+                z-index: 100;
 
                 .menu {
                     display: block;
