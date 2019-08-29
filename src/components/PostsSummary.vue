cf89492f59561dd01ab99456208f66740caab584
niyorn <c.niyornram@hotmail.com>
2019-08-29 16:28:23 +0200

    add: PostSummary component

--- src/components/PostsSummary.vue
+++ src/components/PostsSummary.vue
@@ -1,75 +1,112 @@
 <template>
     <aside class="posts-summary-container">
         <p class="title">Samenvatting vragen</p>
-        <!-- <div class="summary-container">
+        <div class="summary-container">
             <section class="summary-child-container summary-question">
                 <p class="summary"><span>Vragen gesteld: </span><span class="count">{{question.total}}</span></p> 
+                <p class="summary"><span>Vragen beantwoord: </span><span class="count">{{question.totalAnswered}}</span></p>                 
                 <p class="summary"><span>Vragen onbeantwoord: </span><span class="count">{{question.totalUnanswered}}</span></p>                 
             </section>
             <section class="summary-child-container summary-answer">
-                <p class="summary"><span>Antwoord is volledig: </span><span class="count">{{answerStatus.complete}}</span></p> 
-                <p class="summary"><span>Antwoord is onvolledig: </span><span class="count">{{answerStatus.inComplete}}</span></p> 
-                <p class="summary"><span>Antwoord volstaat niet: </span><span class="count">{{answerStatus.inSufficient}}</span></p> 
+                <p class="summary"><span>Antwoord totaal: </span><span class="count">{{answer.total}}</span></p> 
+                <p class="summary"><span>Antwoord is volledig: </span><span class="count">{{answer.complete}}</span></p> 
+                <p class="summary"><span>Antwoord is onvolledig: </span><span class="count">{{answer.inComplete}}</span></p> 
+                <p class="summary"><span>Antwoord volstaat niet: </span><span class="count">{{answer.inSufficient}}</span></p> 
             </section>
-        </div> -->
+        </div>
     </aside>
 </template>
 
 
+<static-query>	
+    query Posts {	
+			allPost {
+				edges {
+					node {
+						id
+						title
+						typePost
+						status
+						answered
+					}
+				}
+			}
+    }	
+</static-query>
+
 <script>
 export default {
     name: "PostsSummary",
-    // computed: {
-    //     question() {
-    //         const data = this.$static.question
+    computed: {
+        question() {
+            const data = this.$static.allPost.edges
 
-    //         //count how many items are unanswered
-    //         let countUnanswered = 0;
-    //         this.$static.question.edges.forEach(element => {
-    //             if(element.node.unanswered === true) {
-    //                 countUnanswered++
-    //             }
-    //         })
-    //         const question = {
-    //             total: data.totalCount,
-    //             totalUnanswered: countUnanswered
-    //         }
+						//Get all post that are a Question
+						const getPostQuestion = data.filter(post=> {
+							return post.node.typePost === "Vraag"
+						})
 
-    //         return question
-    //     },
-    //     answerStatus() {
-    //         const data = this.$static.answer.edges
-    //         let countComplete = 0
-    //         let countIncomplete = 0
-    //         let countInSufficient = 0
-    //         const status = {
-    //             complete: 1,
-    //             inComplete: 2,
-    //             inSufficient: 3
-    //         }
 
-    //         data.forEach(element => {
-    //             const answerStatus = element.node.status
-    //             if(answerStatus === status.complete) {
-    //                 countComplete++
-    //             }
-    //             else if (answerStatus === status.inComplete) {
-    //                 countIncomplete++
-    //             }
-    //             else if (answerStatus === status.inSufficient) {
-    //                 countInSufficient++
-    //             }
-    //         })
+						//Get all post that is a Question and is still answered
+						const getQuestionAnswered = getPostQuestion.filter(post=> {
+							return post.node.answered === true
+						})
+							
 
-    //         const answerTotalStatus = {
-    //             complete: countComplete,
-    //             inComplete: countIncomplete,
-    //             inSufficient: countInSufficient
-    //         }
+						//Get all post that is a Question and is still unanswered
+						const getQuestionUnanswered = getPostQuestion.filter(post=> {
+							return post.node.answered === false
+						})
+							
 
-    //         return answerTotalStatus
-    //     }
-    // }
+            const question = {
+								total: getPostQuestion.length,
+								totalAnswered: getQuestionAnswered.length,
+                totalUnanswered: getQuestionUnanswered.length
+            }
+
+            return question
+				},
+				answer() {
+					const data = this.$static.allPost.edges
+					const status = {
+						notApplicable : 1,
+						complete: 2,
+						inComplete: 3,
+						notSufficient: 4
+					}
+
+					//Get all post that are a Question
+					const getPostAnswer = data.filter(post => {
+						return post.node.typePost === "Antwoord" && post.node.status !== status.notApplicable
+					})
+
+					//Get all post that is a Question and is still answered
+					const getPostComplete = getPostAnswer.filter(post => {
+						return post.node.status === status.complete
+					})
+					//Get all post that is a Question and is still answered
+					const getPostInComplete = getPostAnswer.filter(post => {
+						return post.node.status === status.inComplete
+					})
+					//Get all post that is a Question and is still answered
+					const getPostNotSufficient = getPostAnswer.filter(post => {
+						return post.node.status === status.notSufficient
+					})
+							
+
+
+
+					const postAnswer = {
+						total : getPostAnswer.length,
+						complete: getPostComplete.length,
+						inComplete: getPostInComplete.length,
+						inSufficient: getPostNotSufficient.length
+					}
+
+					return postAnswer
+				}
+		}
 }
 </script>
 
